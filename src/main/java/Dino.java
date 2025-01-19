467db0fd0b4986b387b84a83da531411197dcc3b
brand0nnn <136957025+brand0nnn@users.noreply.github.com>
2025-01-19 18:14:55 +0800

    Add Rename, Greet, Exit

---
+++ src/main/java/Dino.java
@@ -0,0 +1,14 @@
+public class Dino {
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello! I'm Dino");
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+}
