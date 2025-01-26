52d5868081655a9002fb0fc899e1695c2ba5f29d
brand0nnn <136957025+brand0nnn@users.noreply.github.com>
2025-01-26 17:39:48 +0800

    Add ability to mark and unmark tasks

--- src/main/java/Dino.java
+++ src/main/java/Dino.java
@@ -20,11 +20,14 @@ public class Dino {
 
     public static void main(String[] args) {
         sayHello();
+
         String[] list = new String[100];
+        boolean[] checked = new boolean[100];
         int size = 0;
         boolean goodbye = false;
         String line;
         Scanner in = new Scanner(System.in);
+
         while (!goodbye) {
             line = in.nextLine();
             if (line.equals("bye")) {
@@ -32,10 +35,47 @@ public class Dino {
             }
             else if (line.equals("list")) {
                 printLine();
+                char check;
                 for (int i = 0; i < size; i++) {
-                    System.out.println(i+1 + ". " + list[i]);
+                    if (checked[i]) {
+                        check = 'X';
+                    }
+                    else {
+                        check = ' ';
+                    }
+                    System.out.println(i+1 + ".[" + check + "] " + list[i]);
                 }
                 printLine();
+            }
+            else if (line.startsWith("mark ")) {
+                printLine();
+                String number = line.substring(5);
+                int val = Integer.parseInt(number);
+                if (checked[val-1]) {
+                    System.out.println("Oops, this item has already been marked!");
+                    printLine();
+                }
+                else {
+                    checked[val - 1] = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [X] " + list[val - 1]);
+                    printLine();
+                }
+            }
+            else if (line.startsWith("unmark ")) {
+                printLine();
+                String number = line.substring(7);
+                int val = Integer.parseInt(number);
+                if (!checked[val-1]) {
+                    System.out.println("Oops, this item is already unmarked!");
+                    printLine();
+                }
+                else {
+                    checked[val-1] = false;
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  [ ] " + list[val-1]);
+                    printLine();
+                }
             }
             else {
                 printLine();
@@ -45,6 +85,7 @@ public class Dino {
                 printLine();
             }
         }
+
         sayBye();
     }
 }
