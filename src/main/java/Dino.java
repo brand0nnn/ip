49cc3c4a68c0a00ded0e0d7e4489b65f9e7f612b
brand0nnn <136957025+brand0nnn@users.noreply.github.com>
2025-01-26 16:42:35 +0800

    Add function to echo input

--- src/main/java/Dino.java
+++ src/main/java/Dino.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
+
 public class Dino {
     public static void printLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void main(String[] args) {
+    public static void sayHello() {
         printLine();
         System.out.println("Hello! I'm Dino");
         System.out.println("What can I do for you?");
         printLine();
+    }
+
+    public static void sayBye() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        boolean goodbye = false;
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (!goodbye) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                goodbye = true;
+            }
+            else {
+                printLine();
+                System.out.println(line);
+                printLine();
+            }
+        }
+        sayBye();
     }
 }
