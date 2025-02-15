9586e42af669bbef158a62973f93eca3071f916c
brand0nnn <136957025+brand0nnn@users.noreply.github.com>
2025-02-15 19:58:32 +0800

    Add ability to save data to a save file

--- src/main/java/dino/Dino.java
+++ src/main/java/dino/Dino.java
@@ -1,8 +1,12 @@
 package dino;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import dino.exception.DinoException;
 import dino.exception.ExceptionMessage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Dino {
     public static void printLine() {
@@ -31,39 +35,39 @@ public class Dino {
         printLine();
     }
 
-    public static void displayList(Task[] tasks, int size) {
+    public static void displayList(ArrayList<Task> tasks) {
         printLine();
-        for (int i = 0; i < size; i++) {
-            String date = tasks[i].getDate();
-            System.out.println(i+1 + ".[" + tasks[i].getTypeIcon() + "][" + tasks[i].getStatusIcon() + "] " + tasks[i].description + date);
+        for (int i = 0; i < tasks.size(); i++) {
+            String date = tasks.get(i).getDate();
+            System.out.println(i+1 + ".[" + tasks.get(i).getTypeIcon() + "][" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).description + date);
         }
         printLine();
     }
 
-    public static void markItem(Task[] tasks, String line, boolean mark) throws DinoException {
+    public static void markItem(ArrayList<Task> tasks, String line, boolean mark) throws DinoException {
         printLine();
         int startIndex = mark ? 5 : 7;
         String number = line.substring(startIndex);
         int val = Integer.parseInt(number);
-        if (mark && tasks[val-1].isDone) {
+        if (mark && tasks.get(val-1).isDone) {
             throw new DinoException(ExceptionMessage.ITEM_MARKED);
         }
-        if (!mark && !tasks[val-1].isDone) {
+        if (!mark && !tasks.get(val-1).isDone) {
             throw new DinoException(ExceptionMessage.ITEM_UNMARKED);
         }
         if (mark) {
-            tasks[val-1].markAsDone();
+            tasks.get(val-1).markAsDone();
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  [X] " + tasks[val-1].description);
+            System.out.println("  [X] " + tasks.get(val-1).description);
         } else {
-            tasks[val-1].unmarkAsDone();
+            tasks.get(val-1).unmarkAsDone();
             System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("  [ ] " + tasks[val-1].description);
+            System.out.println("  [ ] " + tasks.get(val-1).description);
         }
         printLine();
     }
 
-    public static void addItem(Task[] tasks, int size, String line, Type type) throws DinoException {
+    public static void addItem(ArrayList<Task> tasks, String line, Type type) throws DinoException {
         String task = "";
         String[] parts;
         switch (type) {
@@ -72,7 +76,7 @@ public class Dino {
             if (task.isEmpty()) {
                 throw new DinoException(ExceptionMessage.EMPTY_COMMAND);
             }
-            tasks[size] = new Todo(task);
+            tasks.add(new Todo(task));
             break;
         case DEADLINE:
             if (line.split(" ", 2)[1].trim().isEmpty()) {
@@ -83,7 +87,7 @@ public class Dino {
             if (parts[1].trim().isEmpty()) {
                 throw new DinoException(ExceptionMessage.NO_BY_DATE);
             }
-            tasks[size] = new Deadline(task, parts[1].trim());
+            tasks.add(new Deadline(task, parts[1].trim()));
             break;
         case EVENT:
             if (line.split(" ", 2)[1].trim().isEmpty()) {
@@ -95,43 +99,59 @@ public class Dino {
             if (dates[1].trim().isEmpty()) {
                 throw new DinoException(ExceptionMessage.NO_TO_DATE);
             }
-            tasks[size] = new Event(task, dates[0].trim(), dates[1].trim());
+            tasks.add(new Event(task, dates[0].trim(), dates[1].trim()));
             break;
         }
         printLine();
         System.out.println("Got it. I've added this task:");
-        System.out.println("  [" + tasks[size].getTypeIcon() + "][ ] " + task);
-        System.out.println("Now you have " + (size + 1) + " tasks in the list.");
+        System.out.println("  [" + tasks.get(tasks.size() - 1).getTypeIcon() + "][ ] " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
         printLine();
+    }
+
+    public static void saveToFile(String filePath, ArrayList<Task> tasks) {
+        File dir = new File("./data");
+        if (!dir.exists()) {
+            if (!dir.mkdir()) {
+                System.out.println("Error: Failed to make directory");
+            }
+        }
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            for (Task task : tasks) {
+                fw.write("[" + task.getTypeIcon() + "][" + task.getStatusIcon() + "] " + task.description + task.getDate() + System.lineSeparator());
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
         sayHello();
 
-        Task[] tasks = new Task[100];
-        int size = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         boolean isExit = false;
         String line;
         Scanner in = new Scanner(System.in);
+        String filePath = "data/savefile.txt";
 
         while (!isExit) {
             line = in.nextLine();
             if (line.equals("bye")) {
                 isExit = true;
             } else if (line.equals("list")) {
-                displayList(tasks, size);
+                displayList(tasks);
             } else if (line.startsWith("todo ")) {
                 try {
-                    addItem(tasks, size, line, Type.TODO);
-                    size++;
+                    addItem(tasks, line, Type.TODO);
                 } catch (DinoException e) {
                     System.out.println(e.getMessage());
                     System.out.println("Check whether your command is correct: todo {task name}");
                 }
             } else if (line.startsWith("deadline ")) {
                 try {
-                    addItem(tasks, size, line, Type.DEADLINE);
-                    size++;
+                    addItem(tasks, line, Type.DEADLINE);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("INPUT ERROR: dino.Deadline end date not specified.");
                     System.out.println("Check whether your command is correct: deadline {task name} /by {end date/time}");
@@ -141,8 +161,7 @@ public class Dino {
                 }
             } else if (line.startsWith("event ")) {
                 try {
-                    addItem(tasks, size, line, Type.EVENT);
-                    size++;
+                    addItem(tasks, line, Type.EVENT);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("INPUT ERROR: Improper start and end date specified.");
                     System.out.println("Check whether your command is correct: event {event name} /from {start date/time} /to {end date/time}");
@@ -172,7 +191,7 @@ public class Dino {
                 printLine();
             }
         }
-
+        saveToFile(filePath, tasks);
         sayBye();
     }
 }
