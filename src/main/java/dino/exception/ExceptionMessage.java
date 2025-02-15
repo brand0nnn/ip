ac8ca91934ea08fec8d39bfedbacfda8574f6e46
brand0nnn <136957025+brand0nnn@users.noreply.github.com>
2025-02-15 18:15:12 +0800

    Add command to delete tasks

--- src/main/java/dino/exception/ExceptionMessage.java
+++ src/main/java/dino/exception/ExceptionMessage.java
@@ -1,11 +1,13 @@
 package dino.exception;
 
 public enum ExceptionMessage {
-    EMPTY_COMMAND("INPUT ERROR: dino.Task cannot be empty."),
+    EMPTY_COMMAND("INPUT ERROR: Task cannot be empty."),
+    EMPTY_INDEX("INPUT ERROR: Index cannot be empty"),
     ITEM_MARKED("Oops, this item has already been marked!"),
     ITEM_UNMARKED("Oops, this item has already been unmarked!"),
     NO_BY_DATE("INPUT ERROR: No deadline end date specified"),
-    NO_TO_DATE("INPUT ERROR: No event end date specified");
+    NO_TO_DATE("INPUT ERROR: No event end date specified"),
+    INVALID_INDEX("INPUT ERROR: Index specified does not exist");
 
     private final String message;
 
